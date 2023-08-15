99129300d284514ddb5492d16f44548faa8a67e1
NohaZak <nohazakaria55@gmail.com>
2023-08-15 03:36:07 -0700

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-       char i, h;
-       
-       for (i = 0; i <= 9; i++)
-       {
-	       for (h = 'a'; h <= 'z'; h++)
-	       {
-		       _putchar(i);
-	       }
-	       _putchar('\n');
-       }
+	char i, h;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (h = 'a'; h <= 'z'; h++)
+		{
+			_putchar(h);
+		}
+		_putchar('\n');
+	}
 }

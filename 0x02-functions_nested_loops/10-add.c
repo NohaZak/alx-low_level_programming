7461087ef30b771a6d436623e3c571c34082bafe
NohaZak <nohazakaria55@gmail.com>
2023-08-15 08:51:39 -0700

    add 2 intgeres

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - main function used for adding
+ *@e: first integer
+ *@f: second integer
+ *
+ * Return: sum of e + f
+ */
+
+int add(int e, int f)
+{
+	return (e + f);
+}

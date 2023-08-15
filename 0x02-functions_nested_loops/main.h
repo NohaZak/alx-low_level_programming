443318b7092d1d6348d52f33f3a93b8dfb8d3d43
NohaZak <nohazakaria55@gmail.com>
2023-08-15 02:09:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints the character to standard output
+ *@c: character to be printed
+ * 
+ * Return: Always return 1 or 0
+ */
+
+int _putchar(char c);
+
+#endif

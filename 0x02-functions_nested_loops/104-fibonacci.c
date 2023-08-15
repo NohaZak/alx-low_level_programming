b20ec4af8092d8d1bc2d943a095da6c95a9818d9
NohaZak <nohazakaria55@gmail.com>
2023-08-15 12:29:44 -0700

    fibonacci 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
+ * numLength - returns the string's length
+ * @num: operand number
+ *
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - main entry
  *
  * Description: prints the first 98 Fibonacci numbers,
@@ -11,20 +32,32 @@
 
 int main(void)
 {
-	int a = 1, b = 2;
+	int count, init0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	printf("%d, %d", a, b);
-
-	for (int i = 2; i < 98; i++)
+	for (count = 1; count <= 98; count++)
 	{
-		int c = a + b;
+		if (f1o > 0)
+			printf("%lu", f1o);
+		init0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && init0s > 0)
+		{
+			printf("%d", 0);
+			init0s--;
+		}
+		printf("%lu", f1);
 
-		printf(", %d", c);
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
 
-		a = b;
-		b = c;
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
-

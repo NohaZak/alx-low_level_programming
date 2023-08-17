a7fb07dfb75aa100e82639e362f134d66be693f5
NohaZak <nohazakaria55@gmail.com>
2023-08-17 05:26:35 -0700

    most numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,10 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-	for (i = 0; i <= 14; i++)
-		_putchar(i + '0');
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }

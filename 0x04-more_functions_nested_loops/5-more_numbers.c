e297438a97420f08fc33e4ce5deaa34bbececdb6
NohaZak <nohazakaria55@gmail.com>
2023-08-17 05:22:56 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: numbers from 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+	for (i = 0; i <= 14; i++)
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}

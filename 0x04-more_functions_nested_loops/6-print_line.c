ec43a848df435de21ffcc6f8fd8f64a13c487561
NohaZak <nohazakaria55@gmail.com>
2023-08-17 06:41:52 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints line based on given n
+ *
+ * @n: paramater
+ *
+ * Return: drawn line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < n; i++)
+	{
+		_putchar('_');
+		_putchar('\n');
+		if (i <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+}

fd09c362c61a847a938d6370001e01e2348035a5
NohaZak <nohazakaria55@gmail.com>
2023-08-23 10:01:20 -0700

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * isLower - determines lowercase
- *
- * @a: character
- *
- * Return: 1 if success, 0 if fail
- */
-
-int isLower(char a)
-{
-	return (a >= 97 && a <= 122);
-}
-
-/**
- * isDelimiter - determines delimiter
- *
- * @d: character
- *
- * Return: 1 if success, 0 if fail
- */
-
-int isDelimiter(char d)
-{
-	int s;
-	char delimiter[] = " ;\t\n,.!?\"(){}";
-
-	for (s = 0; s < 12; s++)
-	{
-		if (d == delimiter[s])
-			return (1);
-	}
-	return (0);
-}
 
 /**
  * cap_string - a function that capitalizes all words of a string
@@ -45,25 +11,29 @@ int isDelimiter(char d)
 
 char *cap_string(char *str)
 {
-	char *p = str;
-	int foundDeli = 1;
+	int index = 0;
 
-	while (*str)
+	while (str[index])
 	{
-		if (isDelimiter(*str))
-		{
-			foundDeli = 1;
-		}
-		else if (isLower(*str) && foundDeli)
-		{
-			*str -= 32;
-			foundDeli = 0;
-		}
-		else
-		{
-			foundDeli = 0;
-			str++;
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] -= 32;
+		index++;
 	}
-	return (p);
+	return (str);
 }

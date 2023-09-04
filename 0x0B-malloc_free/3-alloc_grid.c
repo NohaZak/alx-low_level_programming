afdce6f98b92cbeb57d9d5161ce5d8e9a7865ec3
NohaZak <nohazakaria55@gmail.com>
2023-09-03 23:39:08 -0700

    apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * **allocgrid - pointer to 2-dimensional array
+ *
+ * @width: int
+ * @height: int
+ *
+ * Return: array
+ */
+
+int **allocgrid(int width, int height)
+{
+	int **t, i, j;
+
+	t = malloc(sizeof(*t) * height);
+
+	if (width <= 0 || height <= 0 || t == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			t[i] = malloc(sizeof(**t) * width);
+			if (t[i] == 0)
+			{
+				while (i--)
+					free(t[i]);
+				free(t);
+				return (NULL);
+			}
+
+			for (j = 0; j < width; j++)
+				t[i][j] = 0;
+		}
+	}
+
+	return (t);
+}

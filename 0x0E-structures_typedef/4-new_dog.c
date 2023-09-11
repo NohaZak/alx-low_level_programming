53758787abae0ab5dbff8fe44ff98fac9ec03da0
NohaZak <nohazakaria55@gmail.com>
2023-09-11 02:56:38 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,13 +58,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if ((*dog)->name == NULL)
+	if ((*dog).name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if ((*dog)->owner == NULL)
+	if ((*dog).owner == NULL)
 	{
 		free(dog->name);
 		free(dog);

25503c2ea17246b38abd90fe4fbac11c8cbfcfbe
NohaZak <nohazakaria55@gmail.com>
2023-09-13 04:36:25 -0700

    function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main function
  *
@@ -9,18 +10,33 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *ptr = (char *)main;
-	int r;
+	int b, i;
+	char *arr;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	r = atoi(argv[1]);
-	if (r < 0)
-		printf("Error\n"), exit(2);
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+	       	exit(2);
+	}
 
-	while (b--)
-		printf("%02hhx%s", *ptr++, r ? " " : "\n");
+	arr = (char *)main;
+
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }

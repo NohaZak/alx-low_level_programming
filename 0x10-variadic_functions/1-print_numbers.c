2c95bccbced1cd80b1c66080b1360cdb2986719a
NohaZak <nohazakaria55@gmail.com>
2023-09-14 07:09:38 -0700

    print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,15 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int a = v;
+	int a = n;
 	va_list ap;
 
-	if (!v)
+	if (!n)
 	{
 		printf("\n");
 		return;
 	}
-	va_start(ap, v);
+	va_start(ap, n);
 	while (a--)
 	{
 		printf("%d%s", va_arg(ap, int),

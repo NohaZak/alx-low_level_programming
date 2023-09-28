dd493474928b4942d256ace4117b2771ab58d86b
NohaZak <nohazakaria55@gmail.com>
2023-09-28 04:44:45 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - print binry strng of numbr
+ * @n: numbr
+ *
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int mem = sizeof(n) * 8, binary = 0;
+
+	while (mem)
+	{
+		if (n & 1L << --mem)
+		{
+			_putchar('1');
+			binary++;
+		}
+		else if (binary)
+		{
+			_putchar('0');
+		}
+	}
+	if (!binary)
+	{
+		_putchar('0');
+	}
+}

2e6d8371a895dfda8eba67eb69d3389839746ac1
NohaZak <nohazakaria55@gmail.com>
2023-10-02 11:10:28 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,45 @@
 #include "main.h"
 
-#define USGE "Usage: cp file_from file_to\n"
-#define ERROR_READ "Error: Can't read from file %s\n"
-#define ERROR_WRITE "Error: Can't write to %s\n"
-#define ERROR_CLOSE "Error: Can't close fd %d\n"
-#define PRMSSINS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
- * _copy_content - program
+ * main - entry poinmt
  * @ac: count
  * @av: vector
  *
  * Return: 1 or 0
  */
-int _copy_content(int ac, char **av)
+int main(int ac, char **av)
 {
-	int frm_g = 0, to_g = 0;
+	int from_g = 0, to_g = 0;
 	ssize_t m;
 	char buffer[READ_BUFFER_SIZE];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, USGE), exit(97);
-	frm_g = open(av[1], O_RDONLY);
-	if (frm_g == -1)
-		dprintf(STDERR_FILENO, ERROR_READ, av[1]), exit(98);
-	to_g = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PRMSSINS);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	from_g = open(av[1], O_RDONLY);
+	if (from_g == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	to_g = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_g == -1)
-		dprintf(STDERR_FILENO, ERROR_WRITE, av[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((m = read(frm_g, buffer, READ_BUFFER_SIZE)) > 0)
+	while ((m = read(from_g, buffer, READ_BUFFER_SIZE)) > 0)
 		if (write(to_g, buffer, m) != m)
-			dprintf(STDERR_FILENO, ERROR_WRITE, av[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (m == -1)
-		dprintf(STDERR_FILENO, ERROR_READ, av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	frm_g = close(frm_g);
+	from_g = close(from_g);
 	to_g = close(to_g);
-	if (frm_g)
-		dprintf(STDERR_FILENO, ERROR_CLOSE, frm_g), exit(100);
+	if (from_g)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_g), exit(100);
 	if (to_g)
-		dprintf(STDERR_FILENO, ERROR_CLOSE, frm_g), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_g), exit(100);
 
 	return (EXIT_SUCCESS);
 }

d0051bbf9d15d808b282ed2d0ebfa001ae62498c
NohaZak <nohazakaria55@gmail.com>
2024-02-01 05:34:52 -0800

    Singly linked list project

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - add node to head of list
+ * @head: ptr tp head nde
+ * @str: str field of node
+ *
+ * Return: list size
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newHead = malloc(sizeof(list_t));
+
+	if (!head || !newHead)
+		return (NULL);
+	if (str)
+	{
+		newHead->str = strdup(str);
+		if (!newHead->str)
+		{
+			free(newHead);
+			return (NULL);
+		}
+		newHead->len = strlen(newHead->str);
+	}
+
+	newHead->next = *head;
+	*head = newHead;
+	return (newHead);
+}
